6904d99b91e1230f9a9f52445f72f25286a33c79
Guillaume Hascout <110487415+GuillaumeHascout@users.noreply.github.com>
2022-08-29 19:35:23 +0200

    ReadMe

---
+++ src/main/java/com/Example.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World22");
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+
+    }
+}
